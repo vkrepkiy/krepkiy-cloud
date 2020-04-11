24df2c072f8dc5a5e122a6f4e6df50adbca2f89c
Valentin <valkrepkiy@gmail.com>
2020-04-11 13:04:47 +0300

    add animejs

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from "../../modules/core/component";
-import { Router } from "../../modules/core/router/router";
+import { Component, ComponentInterface } from "../../modules/core/component";
+import anime from "animejs";
+
+const grid = [50, 50];
+const gridItems = grid[0] * grid[1];
 
 @Component({
   tag: "vk-home",
   template: require("./home.html"),
   style: require("./home.css"),
 })
-export class HomeElement extends HTMLElement {
-  constructor(public router: Router = new Router()) {
-    super();
+export class HomeElement extends HTMLElement implements ComponentInterface {
+  componentConnected() {
+    anime({
+      targets: this.shadowRoot!.querySelector("h1"),
+      translateX: "0vw",
+      rotate: "1turn",
+      duration: 2000,
+      delay: 0,
+    });
   }
 }
 
