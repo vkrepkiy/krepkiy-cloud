68ed43bfc9e442b053c288bd3f57c07ad9523fd4
Valentin <valkrepkiy@gmail.com>
2020-04-18 01:09:36 +0300

    improve a bit tag-cloud

--- src/app/modules/tag-cloud/tag-cloud.ts
+++ src/app/modules/tag-cloud/tag-cloud.ts
@@ -1,5 +1,18 @@
 import { Component, ComponentInterface } from "../core/component";
 
+interface AnimationConfig {
+  scale: number;
+  speed: number;
+  rotation: number;
+  translateX: number;
+}
+
+const enum AnimationState {
+  default,
+  initial,
+  second,
+}
+
 @Component({
   tag: "vk-tag-cloud",
   template: require("./tag-cloud.html"),
@@ -8,49 +21,96 @@ import { Component, ComponentInterface } from "../core/component";
 })
 export class TagCloudElement extends HTMLElement implements ComponentInterface {
   public targetFps = 60;
-  public calculationFps = 2;
+  public calculationFps = 0.5;
   public calculateEachMs = 1000 / this.calculationFps; // should be 500
   public shouldRenderMs = 1000 / this.targetFps; // should be 100
   public smoothForMs = this.calculateEachMs - this.shouldRenderMs; // 400?
   public smoothForSec = Math.round(this.smoothForMs / 10) / 100; // should be 0.4 ??
   public degreePerSec = 5;
-
-  protected items: HTMLElement[];
-
-  private lastAnimationFrameTs = 0;
+  public size: number;
+  protected items: [HTMLElement, AnimationConfig][];
+  private lastAnimationFrameTs: number = -1 - this.calculateEachMs;
 
   componentConnected() {
-    this.items = this.initItems();
-
-    requestAnimationFrame(this.requestAnimationFrame);
+    // Init props
+    this.size = Math.min(this.offsetHeight, this.offsetWidth) - 400;
+    // Setup logic
+    this.items = this.getItems();
+    // Run animation with first-time flag
+    requestAnimationFrame((time) =>
+      this.requestAnimationFrame(time, AnimationState.initial)
+    );
   }
 
-  initItems(): HTMLElement[] {
-    return Array.from(this.shadowRoot!.querySelectorAll("li")).map((el, i) => {
-      el.style.position = "absolute";
-      el.style.transition = `all linear ${this.smoothForSec}s`;
-      return this.setRandomPosition(el, i);
+  /**
+   * Get animated items and configs
+   */
+  getItems(): [HTMLElement, AnimationConfig][] {
+    return Array.from(this.shadowRoot!.querySelectorAll("li")).map((el) => {
+      return this.initItem(el);
     });
   }
 
-  setRandomPosition(el: HTMLElement, i: number): HTMLElement {
-    el.dataset.rotationOffset = Math.floor(Math.random() * 360).toString();
-    el.style.transform = `rotate(${el.dataset.rotationOffset}deg) translateX(100%) rotate(-${el.dataset.rotationOffset}deg)`;
-    return el;
+  initItem(el: HTMLElement): [HTMLElement, AnimationConfig] {
+    const config: AnimationConfig = this.getRandomConfig();
+    return [el, config];
   }
 
-  runAnimationFrame() {
-    for (const el of this.items) {
-      let nextVal =
-        parseInt(el.dataset.rotationOffset!, 10) + this.degreePerSec;
-      el.dataset.rotationOffset = nextVal.toString();
-      el.style.transform = `rotate(${nextVal}deg) translateX(100%) rotate(-${nextVal}deg)`;
+  getRandomConfig(): AnimationConfig {
+    // Speed from 0 to 1
+    const randomSeed = Math.round(Math.random() * 1000) / 1000;
+    return {
+      scale: Math.max(0.7, randomSeed * 2),
+      speed: Math.max(5, randomSeed * 10),
+      rotation: Math.floor(Math.random() * 360),
+      translateX: Math.max(100, Math.round(randomSeed * this.size)),
+    };
+  }
+
+  transformEl(el, config: AnimationConfig): void {
+    el.style.transform = `scale(${config.scale}) rotate(${config.rotation}deg) translateX(${config.translateX}px) rotate(-${config.rotation}deg)`;
+  }
+
+  /**
+   * This method mutates config object, be aware
+   */
+  updateConfigForNextFrame(config: AnimationConfig): AnimationConfig {
+    config.rotation = config.rotation + this.degreePerSec * config.speed;
+    return config;
+  }
+
+  runAnimationUpdate(setTransitions?: boolean) {
+    for (const [el, config] of this.items) {
+      if (setTransitions) {
+        this.setTransition(el);
+      }
+      this.transformEl(el, this.updateConfigForNextFrame(config));
     }
   }
 
-  requestAnimationFrame = (time: number): void => {
-    if (time - this.lastAnimationFrameTs > this.calculateEachMs) {
-      this.runAnimationFrame();
+  // CSS transition is great for smoothing rare js calculations
+  setTransition(el: HTMLElement): void {
+    el.style.transition = `all linear ${this.smoothForSec}s`;
+  }
+
+  /**
+   * First draw should be done without smoothing (if we don't want "appearing" effect).
+   * Second animation update should be done strictly on next animationFrame
+   * after applying transition property
+   */
+  requestAnimationFrame = (time: number, state?: AnimationState): void => {
+    // Make shure first animation frame will be rendered on time
+    if (state === AnimationState.initial) {
+      this.runAnimationUpdate();
+      requestAnimationFrame((time) =>
+        this.requestAnimationFrame(time, AnimationState.second)
+      );
+      return;
+    } else if (state === AnimationState.second) {
+      this.runAnimationUpdate(true);
+      this.lastAnimationFrameTs = time;
+    } else if (time - this.lastAnimationFrameTs > this.calculateEachMs) {
+      this.runAnimationUpdate();
       this.lastAnimationFrameTs = time;
     }
 
